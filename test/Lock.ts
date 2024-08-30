3e089854d3d7c95587335f66295e2c423ed65137
Mohammed aljasser <justmo59@gmail.com>
2024-08-30 09:03:42 +0300

    init

---
+++ test/Lock.ts
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import hre from "hardhat";
+
+describe("Voting", function () {
+  it("Should mint and vote", async function () {
+    const Voting = await hre.ethers.getContractFactory("VotingSystem");
+    const voting = await Voting.deploy();
+    await voting.waitForDeployment();
+
+    const [owner, Aziz, notVoter] = await hre.ethers.getSigners();
+
+    await voting.mint(owner.address, 100);
+    await voting.mint(Aziz.address, 100);
+
+    await voting.connect(owner).submitProposal("Proposal 1");
+    await voting.connect(Aziz).submitProposal("Proposal 2");
+
+    await voting.connect(owner).vote(0, true);
+    await voting.connect(Aziz).vote(1, false);
+
+    const proposal1 = await voting.proposals(0);
+    const proposal2 = await voting.proposals(1);
+
+    expect(proposal1.yesVotes).to.equal(1);
+    expect(proposal1.noVotes).to.equal(0);
+    expect(proposal2.yesVotes).to.equal(0);
+    expect(proposal2.noVotes).to.equal(1);
+  });
+});
